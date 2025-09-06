9909c712e1f3e6cf5dd45a45e0ee0dd39e73ea76
Juanesarguello <je.arguello26@ciaf.edu.co>
2025-09-06 08:42:38 -0500

    Initializing the new project

---
+++ PythonFrom0/firststeps.py
@@ -0,0 +1,30 @@
+'''nombre = 5
+type(nombre)
+print(nombre)
+nombre = "Juans"
+print(nombre)
+# LOS COMENTARIOS DE 1 SOLA LÍNEA DE CÓDIGO LOS HAGO CON #
+
+# UTILIZO """ PARA INDICAR SALTOS DE LÍNEA
+
+mensaje = """Esto es un mensaje 
+...con tres
+...saltos de linea"""
+print(mensaje)
+if nombre == "Juan":
+    print("Esto es un mensaje")
+
+else:
+    print("No es un mensaje")'''
+
+
+# PARA COMENTAR VARIAS LÍNEAS DE CÓDIGO O UN BLOQUE DE CÓDIGO, UTILIZO ''' CODIGO '''
+
+
+
+
+
+
+
+
+
